d7c1058ea7805d914261ba00bd94a753afcea414
xinxiang123 <2502624892@qq.com>
2022-03-30 18:28:44 +0800

    Push Commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -14,5 +14,7 @@ public class GitTest {
         System.out.println("在hot-fix分支上，第二次修改");
 
         System.out.println("在master分支上第三次修改项目，和hot-fix第二次修改产生冲突");
+
+        System.out.println("push test");
     }
 }
