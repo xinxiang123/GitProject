7ebf03acaa6afd730b9385b108e1ffff39511fba
xinxiang123 <2502624892@qq.com>
2022-03-30 17:36:44 +0800

    Master fourth Commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("第二次修改项目");
 
         System.out.println("在hot-fix分支上，修改项目");
+
+        System.out.println("在master分支上第三次修改项目，和hot-fix第二次修改产生冲突");
     }
 }
